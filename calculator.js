a552cd800106dc43e0b669b525fe651137f9449e
AdityaF25 <116871844+AdityaF25@users.noreply.github.com>
2023-11-29 08:58:40 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,7 @@
+function Calc(){
+    var val1 = document.getElementById("val1").value;
+    var val2 = document.getElementById("val2").value;
+    var opt = document.getElementById("opt").value;
+    var ans = eval(val1 + opt + val2);
+    document.getElementById("ans").value = ans;
+}
